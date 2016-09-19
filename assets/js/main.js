eb1b764cd609889528458a63a846cf220c46cbdc
aasdkl <hsuehchiahao@gmail.com>
2016-09-19 17:03:16 +0800

    add duoshuo comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,19 @@ $(function() {
     {% endif %}
 
     // your scripts
+    
+    // 多说
+    // http://dev.duoshuo.com/docs/50b344447f32d30066000147
+    {% if site.duoshuo_shortname %}
+		var duoshuoQuery = {short_name: '{{ site.duoshuo_shortname }}'};
+		(function() {
+			var ds = document.createElement('script'); ds.type = 'text/javascript'; ds.async = true;
+			ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+			ds.charset = 'UTF-8';
+			(document.getElementsByTagName('head')[0]  || document.getElementsByTagName('body')[0]).appendChild(ds);
+		})();
+    {% endif %}
+
   };
   afterPjax();
 
